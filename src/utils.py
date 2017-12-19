0da732ff5599652056d6971e446d2d0e3e3affcc
eminozkan <eminozkan@linux-eu7q.suse>
2017-12-18 21:08:18 -0500

    added neural networks

--- src/utils.py
+++ src/utils.py
@@ -59,3 +59,40 @@ def regularization_vs_cindex(training_data,validation_data,param_list,number_of_
             
             penalty_cindex.append([penalty,valid_c_index,train_c_index])
     return np.array(penalty_cindex)
+
+def calculate_cutoffs(patient_df,all_genes_list,quantiles):
+    start_time = timeit.default_timer()
+    training_cutoffs = []
+
+    for gene in all_genes_list:
+        best_value = 0
+        best_p = 1
+
+        for quantile in quantiles:
+            current_value = patient_df[gene].quantile(quantile)
+            results = logrank_test(patient_df.loc[patient_df[gene]>current_value,'Days']/365, 
+                                   patient_df.loc[patient_df[gene]<=current_value,'Days']/365, 
+                                   patient_df.loc[patient_df[gene]>current_value,'Vitality'], 
+                                   patient_df.loc[patient_df[gene]<=current_value,'Vitality'], alpha=.99)
+            if results.p_value<best_p:
+                best_value = current_value
+                best_p = results.p_value
+                best_quantile = quantile
+
+        kmf = KaplanMeierFitter()
+        kmf.fit(patient_df.loc[patient_df[gene]>best_value,'Days']/365, 
+                      patient_df.loc[patient_df[gene]>best_value,'Vitality'])
+        higher_median = kmf.median_
+        kmf.fit(patient_df.loc[patient_df[gene]<=best_value,'Days']/365, 
+                      patient_df.loc[patient_df[gene]<=best_value,'Vitality'])
+        lower_median = kmf.median_
+
+        training_cutoffs.append((gene,best_value,best_p,best_quantile,higher_median,lower_median))
+
+
+    training_cutoffs = pd.DataFrame(training_cutoffs,columns=['Symbols',
+                                                             'Expression Cutoffs',
+                                                             'log-rank P Values',
+                                                             'best_quantile','higher_median','lower_median'])
+    print(timeit.default_timer()-start_time)
+    return training_cutoffs
